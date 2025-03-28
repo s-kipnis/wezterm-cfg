5ebfee9328e1e72522651b198061b1adec9bed5c
Sergey Kipnis <skipnis@gmail.com>
2025-03-29 00:40:40 +0100

    base windows support

---
+++ save.ps1
@@ -0,0 +1,5 @@
+$TGT_DIR="$HOME\.config\wezterm\"
+New-Item  -ItemType "Directory" -Path $TGT_DIR -erroraction 'silentlycontinue'
+Push-Location $PSScriptRoot
+robocopy $TGT_DIR "." /s /e /xd .git .github /xo /im
+Pop-Location
